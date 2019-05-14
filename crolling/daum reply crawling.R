40b9703c5edda0ca24777bde7c05ad63c404c01d
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-05-14 21:17:48 +0900

    daum reply crawling.R  File 제거
    코드 초안 제거

--- crolling/daum reply crawling.R
+++
@@ -1,9 +0,0 @@
-if (!requireNamespace("DNH4")){
-  source("https://install-github.me/forkonlp/DNH4")
-}
-
-library(DNH4)
-
-# url <- "http://v.media.daum.net/v/20180513202105651"
-url <- "https://news.v.daum.net/v/20190404074531633"
-k <- DNH4::getComment(url, limit = "all")
