456ff21c56313c70af78878238e3fcf4c8524e39
Jongwook Choi <48971698+Choiwooky@users.noreply.github.com>
2019-05-15 21:53:23 +0900

    하나의 기사url에 대한 다음 댓글 크롤링
    실제로 쓰기 편하도록 다시 작성했고, dplyr 패키지를 적극활용하여 코드를 좀 더 심플하게 만들었다.

--- daum reply crawling.R
+++ daum reply crawling.R
@@ -1,9 +1,32 @@
 if (!requireNamespace("DNH4")){
   source("https://install-github.me/forkonlp/DNH4")
 }
+# DNH4 : 다음 댓글 크롤링을 위한 패키지
+if(!require(dplyr)){
+  install.packages('dplyr')
+}
+# dplyr : 전처리를 위한 패키지
 
 library(DNH4)
+library(dplyr)
 
-# url <- "http://v.media.daum.net/v/20180513202105651"
+#######################################################################
+### 하나의 다음기사 url에 따른 댓글 크롤링
+
 url <- "https://news.v.daum.net/v/20190404074531633"
+# url : 임의의 url입력
+
 k <- DNH4::getComment(url, limit = "all")
+# k : url의 모든 댓글과 다양한 정보를 추출
+k1 <- k %>% select(content, likeCount)
+# k1 : content(댓글), likeCount(공감수)를 추출하여 저장
+k2 <- k1 %>% arrange(desc(likeCount))
+# k2 : likeCount(공감수)를 내림차순으로 정렬
+write.csv(k2, "sample.csv", row.names = TRUE)
+
+#######################################################################
+### dplyr 패키지를 이용해 간편하게 합쳐서 표현
+
+k <- DNH4::getComment(url, limit = "all")
+k.all <- k %>% select(content, likeCount) %>% arrange(desc(likeCount))
+write.csv(k.all, "sample.csv", row.name = TRUE)
