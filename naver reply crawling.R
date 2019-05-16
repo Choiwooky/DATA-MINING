a0d1fc43a2c02a70733050d985977d63de69da3f
Jongwook Choi <48971698+Choiwooky@users.noreply.github.com>
2019-05-16 15:32:47 +0900

    하나의 기사url에 대한 네이버 댓글 크롤링 (수정)
    오타 수정

--- naver reply crawling.R
+++ naver reply crawling.R
@@ -18,10 +18,10 @@ k <- getAllComment(url)
 k.con <- k %>% select(contents, sympathyCount)
 # k.con에 k에 있는 데이터 중 contents(댓글), sympathyCount(공감수)만 추출
 raw.data <- k.con %>% arrange(desc(sympathyCount))
-# raw.data_age에 k.con을 공감순으로 내림차순 한 후 정렬
+# raw.data.age에 k.con을 공감순으로 내림차순 한 후 정렬
 
 ## setwd 이용 후 저장할 위치 확인 후 csv파일 작성
-## write.csv(raw_data, file = "sample.csv", row.names = TRUE)
+## write.csv(raw.data, file = "sample.csv", row.names = TRUE)
 ## 많은 양의 데이터를 한꺼번에 묶을 때는 bind_rows 이용 
 
 ### 패키지 출처 https://forkonlp.github.io/N2H4/
