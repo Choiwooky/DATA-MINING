44ac4a37f609a6f91ab36c64da6bee27044c00c9
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-05-31 15:34:35 +0900

    대선 직전일까지의 날짜 출력(수정)
    박근혜 대통령 탄핵 가결일 ~ 19대 대선 직전일까지의 날짜 출력 함수 추가

--- Presidential Election analysis/Print Date.R
+++ Presidential Election analysis/Print Date.R
@@ -30,3 +30,32 @@ mydate
 # 2012년 대선(18대 대선)
 fdate1718(2012)
 mydate
+
+    
+###########################################################################################################################
+    
+## 박근혜 대통령 탄핵 가결일 2016/12/9 ~ 19대 대선 직전일 2017/5/9
+spdate <- function(year1 = 2016, year2 = 2017) {
+  mydate <<- NULL
+  day <- 1:31
+  for(i in c(12, 1:5)) {
+    if(i == 12) {
+      mydate <<- c(mydate, as.numeric(paste0(year1, i, 0, day[9])), as.numeric(paste0(year1, i, day[10:31])))
+    } else if(i %in% c(1, 3)) {
+      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:31])))
+    } else if(i == 2) {
+      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:28])))
+    } else if(i == 4) {
+      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:30])))
+    } else {
+      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])))
+    }
+  }
+}
+
+# 변수 mydate에 날짜를 저장하고 전역변수로 만들었음.
+# 날짜 사용은 함수를 실행하고 mydate를 호출하면 됨.
+  
+## 결과
+spdate()
+mydate
