a5cc12ea62f37041d904bf8b1254811ca5fa0efc
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-06-06 17:15:58 +0900

    17~19대 대선 날짜 출력 함수.R(수정)
    17, 17, 19대 대선 날짜가 출력되는 전역변수 mydate를 리스트 형식으로 변경하였음.

--- Presidential Election analysis/Print Date.R
+++ Presidential Election analysis/Print Date.R
@@ -39,24 +39,27 @@ mydate
     
 ## 박근혜 대통령 탄핵 가결일 2016/12/9 ~ 19대 대선 직전일 2017/5/9
 spdate <- function(year1 = 2016, year2 = 2017) {
-  mydate <<- NULL
+  mydate <<- list()
   day <- 1:31
   for(i in c(12, 1:5)) {
     if(i == 12) {
-      mydate <<- c(mydate, as.numeric(paste0(year1, i, 0, day[9])), as.numeric(paste0(year1, i, day[10:31])))
-    } else if(i %in% c(1, 3)) {
-      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:31])))
+      mydate[[1]] <<- c(as.numeric(paste0(year1, i, 0, day[9])), as.numeric(paste0(year1, i, day[10:31])))
+    } else if(i == 1) {
+      mydate[[2]] <<- c(as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:31])))
     } else if(i == 2) {
-      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:28])))
+      mydate[[3]] <<- c(as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:28])))
+    } else if(i == 3) {
+      mydate[[4]] <<- c(as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:31])))
     } else if(i == 4) {
-      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:30])))
+      mydate[[5]] <<- c(as.numeric(paste0(year2, 0, i, 0, day[1:9])), as.numeric(paste0(year2, 0, i, day[10:30])))
     } else {
-      mydate <<- c(mydate, as.numeric(paste0(year2, 0, i, 0, day[1:9])))
+      mydate[[6]] <<- as.numeric(paste0(year2, 0, i, 0, day[1:9]))
     }
   }
+  names(mydate) <<- c(month.abb[12], month.abb[1:5])
 }
 
-# 변수 mydate에 날짜를 저장하고 전역변수로 만들었음.
+# 변수 mydate에 날짜를 저장하고 전역변수로 만들었음. list 형식임.
 # 날짜 사용은 함수를 실행하고 mydate를 호출하면 됨.
   
 ## 결과
